6f2bcbaa2b0f63709602a1703bbe235a84849b21
archi829 <arcmin.niaj29@gmail.com>
2025-02-15 16:25:33 +0530

    Delete ps_ext directory

--- ps_ext/popup.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "update_ui") {
-      document.getElementById("status").innerText = request.feedback;
-  }
-});
